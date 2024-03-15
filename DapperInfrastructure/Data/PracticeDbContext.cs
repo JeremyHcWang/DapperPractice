0d272b54b4adba64c26b464d4902e1d74766ee1e
JeremyHcWang <jemhw9758@gmail.com>
2024-03-15 01:38:55 -0400

    update

--- DapperInfrastructure/Data/PracticeDbContext.cs
+++ DapperInfrastructure/Data/PracticeDbContext.cs
@@ -1,6 +1,23 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
 namespace DapperInfrastructure.Data;
 
 public class PracticeDbContext
 {
-    
+    private readonly IConfiguration configuration;
+    private readonly string connectionString;
+
+    public PracticeDbContext(IConfiguration _configuration)
+    {
+        configuration = _configuration;
+        connectionString = configuration.GetConnectionString("PracticeDbStr");
+    }
+
+    public IDbConnection GetDbConnection()
+    {
+        return new SqlConnection(connectionString);
+    }
+
 }
